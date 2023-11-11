cfaef12ca37071cc9e57da0354669227d11b759c
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 19:26:52 +0000

    Exercise 16

---
+++ exercise16.java
@@ -0,0 +1,14 @@
+public class PlaygroundOrder {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    int a = 2;
+    int b = 3;
+    int c = 4;
+    double result = 3 * a - 2 / (b + 5) + c;
+    System.out.println(result);
+
+    
+    //add code above this line
+  }
+}
