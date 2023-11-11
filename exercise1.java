f36e55b9c73a4de7f8f7fa5a64d9b03853412adf
Nelu Buzu <nelubuzu29@gmail.com>
2023-11-11 14:02:36 +0000

    First exercise

---
+++ exercise1.java
@@ -0,0 +1,22 @@
+
+
+
+
+public class Exercise1 {
+  public static void main(String args[]) {
+    
+    boolean test = Boolean.parseBoolean(args[0]);
+    
+    //add code below this line
+    boolean my_boolean = true ;
+    System.out.print(my_boolean);
+    my_boolean = test ;
+    System.out.print(my_boolean);
+
+    
+    //add code above this line
+  }
+}
+
+
+
