a4c8e3836d389a909f3e6198f0c5a2a52d9b77a5
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 19:30:55 +0000

    Exercise 18

---
+++ exercise18.java
@@ -0,0 +1,11 @@
+public class PlaygroundModulo {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    int modulo = 5 % 2;
+    System.out.println(modulo);
+
+    
+    //add code above this line
+  }
+}
