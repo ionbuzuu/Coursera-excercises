ec47ec9c4c7d5de3d0e04a26b712e22817a8fc7e
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 19:29:56 +0000

    Exercise 17

---
+++ exercise17.java
@@ -0,0 +1,13 @@
+public class PlaygroundMultiply {
+  public static void main(String args[]) {
+    
+    //add code below this line
+
+    int a = 5;
+    int b = 9;
+    System.out.println( a  *   b);
+
+    
+    //add code above this line
+  }
+}
