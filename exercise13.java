6c9009e0b4a1d8737aaa29d9a317d20ef657b3c5
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 18:13:10 +0000

    Exercise 13

---
+++ exercise13.java
@@ -0,0 +1,12 @@
+public class PlaygroundDivision {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+     int a = 5;
+     int b = 2;
+      System.out.println(a / b);
+    
+    //add code above this line
+  }
+}
