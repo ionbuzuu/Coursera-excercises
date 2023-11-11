0c146c4ec9c29a32eb8d6e3102479bcc05a8aec4
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 15:09:21 +0000

    Exercise 5

---
+++ exercise5.java
@@ -0,0 +1,12 @@
+public class PlaygroundAddition {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+double a = 7.0;
+int b = -3;
+System.out.print(a + b);
+    
+    //add code above this line
+  }
+}
