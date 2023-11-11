5bd2f02828eda0b21f472adacd962f0407d521c5
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 15:42:54 +0000

    Exercise 10.2

---
+++ exercise10.2.java
@@ -0,0 +1,13 @@
+public class PlaygroundSubtraction {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    int a = 10;
+     int b = -3;
+       int c = a - b;
+    System.out.println(c);
+    
+    //add code above this line
+  }
+}
