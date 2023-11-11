a6882523d4d04c79385298329146d6b705a11657
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 14:15:20 +0000

    Exercise 4

---
+++ exercise4.java
@@ -0,0 +1,11 @@
+public class Exercise4 {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    System.out.print("Okay, it is time to ");
+    System.out.println("learn about operators.");
+    
+    //add code above this line
+  }
+}
