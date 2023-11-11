865163a31622342eeb7d35aeadccb23870a73090
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 14:08:10 +0000

    Exercise 2

---
+++ exercise2.java
@@ -0,0 +1,20 @@
+public class Exercise2 {
+  public static void main(String args[]) {
+    
+    int first_num = Integer.parseInt(args[0]);
+    int second_num = Integer.parseInt(args[1]);
+    int third_num = Integer.parseInt(args[2]);
+    
+    //add code below this line
+      int  my_int;
+      int  First_num ;
+      System.out.print(first_num);
+      first_num = second_num ; 
+      System.out.print(second_num) ;
+      second_num = third_num ;
+      System.out.print(third_num); 
+      
+    
+    //add code above this line
+  }
+}
