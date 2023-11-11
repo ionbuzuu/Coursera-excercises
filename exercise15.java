8fdcdc3da6fa9f0dbb05167b5faaee5924bdddc0
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 19:12:02 +0000

    Exercise 15

---
+++ exercise15.java
@@ -0,0 +1,15 @@
+public class Exercise1 {
+  public static void main(String args[]) {
+
+    boolean test = Boolean.parseBoolean(args[0]);
+
+    //add code below this line
+   
+    int a = 5;
+    b = "3";
+    System.out.println(a + b);
+
+
+    //add code above this line
+  }
+}
