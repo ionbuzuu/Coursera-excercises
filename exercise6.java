41270b4593787575325dde97c6fc617fa3750236
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 15:15:50 +0000

    Exercise 6

---
+++ exercise6.java
@@ -0,0 +1,13 @@
+public class PlaygroundIncrement {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+   int a = 0;
+    a = a + 1;
+    System.out.print(a);
+   
+    
+    //add code above this line
+  }
+}
