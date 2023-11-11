e9adc71c76af414d4bcb2b9be21bb0027c8dcfe3
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 14:13:48 +0000

    Exercise 3

---
+++ exercise3.java
@@ -0,0 +1,27 @@
+public class Exercise3 {
+  public static void main(String args[]) {
+
+ 
+
+
+
+
+    
+    double one = Double.parseDouble(args[0]);
+    
+    //add code below this line
+    
+    double my_double ;
+
+    my_double = 0.0 ; 
+    
+    System.out.println(my_double);
+
+    my_double = one ;
+
+    System.out.println(my_double);
+    
+    
+    //add code above this line
+  }
+}
