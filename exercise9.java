6582e7449fb946e98326a9f0ccfea50ed336c317
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 15:31:29 +0000

    Exercise 9

---
+++ exercise9.java
@@ -0,0 +1,13 @@
+public class PlaygroundConcatenation {
+  public static void main(String args[]) {
+    
+    //add code below this line
+
+    String a = "This is an ";
+    String b = "example string/text";
+    String c = a += b;
+    String d = "3";  
+    System.out.println(c);
+    
+    //add code above this line
+  }
