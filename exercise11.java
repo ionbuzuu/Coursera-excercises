8d5ba3b59856a6068fc0a18236e07d31ca93382d
Nelu Buzu <cretulea77@gmail.com>
2023-11-11 15:45:04 +0000

    Exercise 11

---
+++ exercise11.java
@@ -0,0 +1,12 @@
+public class PlaygroundSubtraction {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    int a = 10;
+      a--;
+    System.out.println(a);
+    
+    //add code above this line
+  }
+}
