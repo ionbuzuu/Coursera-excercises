9eb9665c16bab39971a0357b005e9f1c3f6b87e4
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 12:25:16 +0000

    Exercise 29

---
+++ exercise29.java
@@ -0,0 +1,10 @@
+public class PlaygroundNot {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+     System.out.println(! true);
+    
+    //add code above this line
+  }
+}
