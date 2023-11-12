9d14b76b341691ad583a3aea6a1b8368cd7ea02f
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 11:48:35 +0000

    Exercise 21

---
+++ exercise21.java
@@ -0,0 +1,13 @@
+public class PlaygroundLessThan {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    int a =  5;
+    int b =  5;
+    System.out.println(a <= b);
+
+    
+    //add code above this line
+  }
+}
+
