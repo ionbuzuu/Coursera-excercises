068e452f2e78a2511448e6fcf4f05badcd9ec139
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 11:52:13 +0000

    Exercise 22

---
+++ exercise22.java
@@ -0,0 +1,12 @@
+public class PlaygroundGreaterThan {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    int a = 9;
+    int b = 17;
+    System.out.println(a >= b);
+     
+    //add code above this line
+  }
+}
