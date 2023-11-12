ed414044ee94a86ec660077277aad983fb023392
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 12:23:45 +0000

    Exercise 28

---
+++ exercise28.java
@@ -0,0 +1,12 @@
+public class PlaygroundOr {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    boolean a = true;
+    boolean b = true; 
+    boolean c = false;
+    System.out.println(a || c || c || c || c);    
+    //add code above this line
+  }
+}
