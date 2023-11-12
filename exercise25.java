a17722d248bcdeadaed19732b04920f9a8af4c7e
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 12:01:23 +0000

    Exercise 25

---
+++ exercise25.java
@@ -0,0 +1,13 @@
+public class PlaygroundAnd {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    boolean a = true;
+    boolean b = true;
+    boolean c = false;
+    System.out.println(a &&  b && c);
+
+    
+    //add code above this line
+  }
+}
