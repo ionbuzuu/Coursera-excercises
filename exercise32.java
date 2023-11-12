76362b83d052550ab1a78aacb348a120c5efa422
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 13:07:46 +0000

    Exercise 32

---
+++ exercise32.java
@@ -0,0 +1,15 @@
+public class LabStrings {
+  public static void main(String args[]) {
+    
+    //add code below this line
+      String string_1 = "hip";
+      String string_2 = string_1 + string_1;
+      String string_3 = "hoo";
+      String string_4 = "ray!";
+      String string_5 = string_3 + string_4;
+      System.out.print(string_2);
+      System.out.println(string_5);
+    
+    //add code above this line
+  }
+}
