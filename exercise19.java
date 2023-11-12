87764843da8a7a3085d584c141680502fbed795a
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 11:12:46 +0000

    Exercise 19

---
+++ exercise19.java
@@ -0,0 +1,14 @@
+public class PlaygroundEquality {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    int a = 5;
+    
+    int b = 5;
+    
+    System.out.println(a == b);
+    
+    //add code above this line
+  }
+}
