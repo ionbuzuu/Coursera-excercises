339aaae9248878299c9ff71110ec24aab9f2845d
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 13:03:49 +0000

    Exercise 31

---
+++ exercise31.java
@@ -0,0 +1,14 @@
+public class LabArithmetic {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    System.out.println(10 + 3);
+    System.out.println(10 - 3);
+    System.out.println(10 * 3);
+    System.out.println(10 / 3);
+    System.out.println(10 % 3);
+    
+    //add code above this line
+  }
+}
