ddd40ae53292533c79076d50c9e03c1892714315
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 12:48:04 +0000

    Exercise 30

---
+++ exercise30and-or.java
@@ -0,0 +1,20 @@
+public class PlaygroundShortCircuiting {
+  public static void main(String args[]) {
+    //add code below this line
+    
+    System.out.println(false
+                                   &&
+                                   true )
+
+                                   System.out.println(
+                                    true
+
+                                            ||
+                                                       false )
+                                                                               )
+    
+    //add code above this line
+
+
+  }
+}
