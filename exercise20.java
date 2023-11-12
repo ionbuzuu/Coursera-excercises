4f9d58d7d271763341412fcb5b6a8a454ffeb5ec
Nelu Buzu <cretulea77@gmail.com>
2023-11-12 11:43:48 +0000

    Exercise 20

---
+++ exercise20.java
@@ -0,0 +1,12 @@
+public class PlaygroundLessThan {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    int a =  5;
+    int b =  6; // you can change to test true or false :)
+    System.out.println(a < b);
+
+    
+    //add code above this line
+  }
+}
