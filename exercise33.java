868c0e048a554edcb1d7193c8fd8126a7fceebc2
Nelu Buzu <cretulea77@gmail.com>
2023-11-13 17:18:39 +0000

    Exercise 33

---
+++ exercise33.java
@@ -0,0 +1,12 @@
+  public class PlaygroundVariables7 {
+  public static void main(String args[]) {
+    //add code below this line
+    
+    String my_variable = "Hello world";
+    System.out.println(my_variable);
+   my_variable = "Goodbye world";
+    System.out.println(my_variable);
+    
+    //add code above this line
+  }
+}
