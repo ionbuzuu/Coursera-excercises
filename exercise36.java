5138ce57d6ffb3922572d2acbcb0ad0439b22228
Nelu Buzu <cretulea77@gmail.com>
2023-11-13 19:23:58 +0000

    Exercise 36

---
+++ exercise36.java
@@ -0,0 +1,13 @@
+public class Exercise4 {
+  public static void main(String args[]) {
+    
+    //write code below this line
+    
+    double a = 7;
+    double b = 2;
+    System.out.println(a / b);
+    
+    //write code above this line
+
+  }
+}
