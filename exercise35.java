df30727aaa83c401f75e0d1a274bddfcd73218fc
Nelu Buzu <cretulea77@gmail.com>
2023-11-13 18:35:40 +0000

    Exercise 35

---
+++ exercise35.java
@@ -0,0 +1,13 @@
+public class Exercise3 {
+  public static void main(String args[]) {
+    
+    //write code below this line
+    String a = "Hello ";
+    String b = "world";
+    System.out.println(a + b);
+
+    
+    //write code above this line
+
+  }
+}
