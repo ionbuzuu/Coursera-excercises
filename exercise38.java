ec324a31475d6864caad2d48b05eec59440265b2
Nelu Buzu <cretulea77@gmail.com>
2023-11-16 15:36:59 +0000

    exercise38.java

---
+++ exercise38.java
@@ -0,0 +1,15 @@
+public class Exercise1 {
+  public static void main(String args[]) {
+    
+    //fix the code below this line
+    
+    int a = 5;
+    int b = 6;
+    
+    //fix the code above this line
+          
+    System.out.println(a * 3 + b - 8 / 2);
+    
+
+  }
+}
