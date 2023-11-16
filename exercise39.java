a3fc40f6e61517f73ca2cf7fd546f18ebf5a9d84
Nelu Buzu <cretulea77@gmail.com>
2023-11-16 15:44:04 +0000

    exercise39.java

---
+++ exercise39.java
@@ -0,0 +1,16 @@
+public class Exercise2 {
+  public static void main(String args[]) {
+    
+    //write code below this line
+    int a = 5;
+    int b = 4;
+    
+    
+    System.out.println( ! (5 < 8) && 6 >= 2 );
+    
+    
+    
+    //write code above this line
+
+  }
+}
