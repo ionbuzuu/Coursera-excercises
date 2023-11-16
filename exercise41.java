d38f6229f28eba1d3503ed7d063fe1562dc22112
Nelu Buzu <cretulea77@gmail.com>
2023-11-16 16:09:15 +0000

    exercise41.java

---
+++ exercise41.java
@@ -0,0 +1,15 @@
+public class PlaygroundIf {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    if (7 != 10){
+
+    
+      System.out.println("The above statement is true");
+      System.out.println("The above statement is still true");
+    }
+    System.out.println("This is not related to the if statement");
+    
+    //add code above this line
+  }
+}
