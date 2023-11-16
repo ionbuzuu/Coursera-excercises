78afbfbdcc46a94c742dadbfb0893c0ddbf56a61
Nelu Buzu <cretulea77@gmail.com>
2023-11-16 16:14:27 +0000

    exercise42.java

---
+++ exercise42.java
@@ -0,0 +1,17 @@
+public class PlaygroundIf {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    int grade = 90;
+
+    if (grade > 70) {
+        System.out.println("Congrats, you passed the class");
+    }
+
+    if(grade < 70) {
+      System.out.println("Condolences, you did not pass the class");
+
+    }
+    //add code above this line
+  }
+}
