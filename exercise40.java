6d8c1a401308e01d0a7097bdc231456eff04c160
Nelu Buzu <cretulea77@gmail.com>
2023-11-16 16:08:45 +0000

    exercise40.java

---
+++ exercise40.java
@@ -0,0 +1,13 @@
+public class PlaygroundIfSyntax {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    if (5 > 4); {
+      System.out.println("1st command if true");
+      System.out.println("2nd command if true ");
+    
+    }
+    System.out.println("I will always print");
+    //add code above this line
+  }
+}
