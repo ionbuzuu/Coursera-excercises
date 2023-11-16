35e16dcf779aa611890ab5dccb658680f9b3b31e
Nelu Buzu <cretulea77@gmail.com>
2023-11-16 15:36:38 +0000

    Exercise 14 v2

---
+++ exercise14.java
@@ -0,0 +1,17 @@
+    
+    public class PlaygroundCasting {
+        public static void main(String args[]) {
+          
+          //add code below this line
+          int numerator = 40;
+          int denominator = 25;
+          System.out.println( numerator / denominator);
+          System.out.println ( (double ) numerator / denominator);
+      
+          
+          //add code above this line
+        }
+      
+
+
+}
