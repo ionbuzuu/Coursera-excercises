d8c88698ea41893b9679b6869dcde9fc258b5322
Nelu Buzu <cretulea77@gmail.com>
2023-11-20 18:37:21 +0000

    exercise44.java

---
+++ exercise44.java
@@ -0,0 +1,12 @@
+public class PlaygroundIfElse {
+  public static void main(String args[]) { 
+    boolean my_bool = true;
+    
+    if (my_bool) {
+      System.out.println("The value of my_bool is true");
+    }
+    else{
+      System.out.println("The value of my_bool is false");
+     }
+  }
+}
