1304b2178f196a662dd3f589b1c72e5f51619c8b
Nelu Buzu <cretulea77@gmail.com>
2023-11-20 18:56:44 +0000

    exercise45.java

---
+++ exercise45.java
@@ -0,0 +1,15 @@
+public class PlaygroundIfElse {
+  public static void main(String args[]) { 
+   int grade = 62;
+  if(grade < 60) {
+  System.out.println("F"); }
+  else if(grade < 70) {
+  System.out.println("D"); }
+  else if(grade < 80) {
+  System.out.println("C"); }
+  else if(grade < 90) {
+  System.out.println("B"); }
+  else if(grade <= 100) {
+  System.out.println("A"); }
+  }
+}
