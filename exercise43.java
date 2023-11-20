bfe707611fcf2c07c1aef64ad14d062bcd88c23b
Nelu Buzu <cretulea77@gmail.com>
2023-11-20 18:24:01 +0000

    exercise43.java

---
+++ exercise43.java
@@ -0,0 +1,14 @@
+public class PlaygroundIfElseSyntax {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    if (5 > 4) { 
+      System.out.println("The boolean expression is true");
+    }
+    else {
+      System.out.println("The boolean expression is false");
+    }
+
+    //add code above this line
+  }
+}
