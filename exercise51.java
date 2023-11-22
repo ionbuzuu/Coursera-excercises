fb9fc61aa287cbbe1842a58b32389f617732baca
Nelu Buzu <cretulea77@gmail.com>
2023-11-22 14:11:36 +0000

    exercise51.java

---
+++ exercise51.java
@@ -0,0 +1,11 @@
+public class Lab_IfElse {
+  public static void main(String args[]) {
+    int x = 10;
+    if (x > 50) {
+      System.out.println(String.valueOf(x) + " is greater tham 50");
+    }
+    else {
+      System.out.println(String.valueOf(x) + " Is less than 50.");
+    }
+  }
+}
