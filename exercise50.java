9589f3392f8f7db3c5e7b51e43554a5a9e7f83fd
Nelu Buzu <cretulea77@gmail.com>
2023-11-22 14:08:45 +0000

    exercise50.java

---
+++ exercise50.java
@@ -0,0 +1,10 @@
+public class Lab_IfStatement {
+  public static void main(String args[]) {
+    int x = 20;
+    if (x < 10) {
+      System.out.println("less than 10");
+    }
+    System.out.println("Add the program continues....");
+  }
+
+}
