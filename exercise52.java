af7324ac33b0e75c5d56bbba6cfb075ad28624d0
Nelu Buzu <cretulea77@gmail.com>
2023-11-22 14:39:56 +0000

    exercise52.java

---
+++ exercise52.java
@@ -0,0 +1,18 @@
+public class Lab_Switch {
+  public static void main(String args[]) {
+    int size = 3; 
+    switch(size) {
+      case 1: System.out.print("Short");
+      break;
+      case 2: System.out.print("Tall");
+      break;
+      case 3: System.out.print("Grande");
+      break;
+      case 4: System.out.print("Venti");
+      break;
+      case 5: System.out.print("Grande");
+      break;
+      default: System.out.print("Grande");
+    }
+  }
+}
