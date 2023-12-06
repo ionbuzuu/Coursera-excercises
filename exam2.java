66e319e593694d21c2115774e60aa79d029777e9
Nelu Buzu <cretulea77@gmail.com>
2023-12-06 16:57:26 +0000

    exam2.java

---
+++ exam2.java
@@ -0,0 +1,12 @@
+public class Exercise4 {
+  public static void main(String args[]) {
+    
+    for (int i = 100; i <= 100; i--) {
+      while (true) {
+        System.out.println("Print me!");
+      break;
+      }
+    break;
+    }
+  }
+}
