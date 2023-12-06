8ae4e7a6877372c3b1d3e98e3625df431813013c
Nelu Buzu <cretulea77@gmail.com>
2023-12-06 16:58:50 +0000

    exercise66.java

---
+++ exercise66.java
@@ -0,0 +1,10 @@
+public class ArrayCreate {
+  public static void main(String args[]) {
+      String a =  "Alan"; 
+      String b =  "Bob";
+      String c =  "Carol";
+      String d =  "David";
+      String e =  "Ellen";
+      System.out.println(b);
+  }
+}
