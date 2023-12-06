d4446feafd71324554acc062b09085b592e0b350
Nelu Buzu <cretulea77@gmail.com>
2023-12-06 16:59:35 +0000

    exercise68.java

---
+++ exercise69.java
@@ -0,0 +1,8 @@
+public class ArrayAccess {
+  public static void main(String args[]) {
+    double[] decimals = new double[2];
+    boolean[] bools = new boolean[2];
+    System.out.println(decimals[2]);
+    System.out.println(bools[0]);
+  }
+}
