444552b24eeea1500b364199ca071cd0b52b00fa
Nelu Buzu <cretulea77@gmail.com>
2023-12-06 16:57:21 +0000

    exam1.java

---
+++ exam1.java
@@ -0,0 +1,15 @@
+public class exam1 {
+  public static void main(String args[]) {
+    
+    //add code below this line
+         for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(".");
+            }
+
+            System.out.println(i);
+         }
+
+    //add code above this line
+  }
+}
