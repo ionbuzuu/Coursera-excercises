14f7958839052f983d728e8b29f837b63ed91689
Nelu Buzu <cretulea77@gmail.com>
2023-12-06 16:58:44 +0000

    exam5.java

---
+++ exam5.java
@@ -0,0 +1,14 @@
+public class Exercise2 {
+  public static void main(String args[]) {
+    
+    String x = args[0];
+    
+    //add code below this line
+
+    for (int i = 0; i < 10; i++) {
+  System.out.println(x);
+    }
+
+    //add code above this line
+  }
+}
