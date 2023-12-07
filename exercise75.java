c3219a35ac72189dcbf4054d147367cb2e387364
Nelu Buzu <cretulea77@gmail.com>
2023-12-07 16:02:42 +0000

    exercise75.java

---
+++ exercise75.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayListIterate {
+  public static void main(String args[]) {
+    int[] grades = {85, 95, 48, 100, 92};
+
+    for (int i = 0; i < grades.length; i++) {
+      System.out.println(grades[i]);
+    }
+  }
+}
