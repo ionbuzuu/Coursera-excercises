7aa8c697a410990d167458f0be42ad1ebad5b73f
Nelu Buzu <cretulea77@gmail.com>
2023-12-07 15:41:46 +0000

    exercise71.java

---
+++ exercise71.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayListAddRemove {
+  public static void main(String args[]) {
+      ArrayList<Integer> numbers = new ArrayList<Integer>();
+      numbers.add(12.3);
+      
+      System.out.println(numbers);
+  }
+}
