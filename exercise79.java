10359ece3fecaf90113c1c36a62f3d5228523491
Nelu Buzu <cretulea77@gmail.com>
2023-12-08 16:42:19 +0000

    exercise79.java

---
+++ exercise79.java
@@ -0,0 +1,9 @@
+public class ArrayModify {
+  public static void main(String args[]) {
+    int[] grades = {85, 95, 48, 100, 92};
+    System.out.println(grades[1]);
+
+    grades[2] = 88;
+    System.out.println(grades[1]);
+  }
+}
