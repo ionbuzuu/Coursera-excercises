91ab8417809cfecd0200568e76d609743d394a5a
Nelu Buzu <cretulea77@gmail.com>
2023-12-08 16:42:26 +0000

    exercise80.java

---
+++ exercise80.java
@@ -0,0 +1,10 @@
+public class ArrayModify {
+  public static void main(String args[]) {
+    String[] family = {"Dad", "Mom", "Brother", "Sister"};
+    int[] age = new int[4];
+    System.out.println(family[0] + " " + age[1]);
+    System.out.println(family[1] + " " + age[1]);
+    System.out.println(family[2] + " " + age[2]);
+    System.out.println(family[3] + " " + age[3]);
+  }
+}
