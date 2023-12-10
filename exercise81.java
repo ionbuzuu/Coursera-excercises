c4ac088603ca5510f13b188882408c673f2a61a5
Nelu Buzu <cretulea77@gmail.com>
2023-12-10 13:25:39 +0000

    exercise81.java

---
+++ exercise81.java
@@ -0,0 +1,8 @@
+public class ArrayIterate {
+  public static void main(String args[]) {
+    String[] friends = {"Alan", "Bob", "Carol", "David", "Ellen", "Fred", "Grace", "Henry", "Ian", "Jen"};
+    for (int i = 0; i < 10 ; i++) { 
+      System.out.println(friends[i]);
+    }
+  }
+}
